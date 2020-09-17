af13c5edd4bc61455b8fc76ff77c0621acdc88b8
sunly000 <1054097910@qq.com>
2020-09-17 18:50:18 +0800

    version3:添加了修改用户信息，删除用户，查询用户数量，通过用户名和性别广泛查询的功能

--- src/main/java/com/sly/mybatis/mapper/UserMapper.java
+++ src/main/java/com/sly/mybatis/mapper/UserMapper.java
@@ -8,17 +8,17 @@ import com.sly.mybatis.pojo.User;
 public interface UserMapper {
 	User getUserById(Integer id);
 
-//	List<User> getUserByUserName(String userName);
+	List<User> getUserByUserName(String userName);
 
 	void insertUser(User user);
-//
-//	void updateUser(User user);
-//
-//	void deleteUser(int id);
-//
-//	Integer getUserCount();
-//
-//	List<User> getUserByPojo(User user);
+
+	void updateUser(User user);
+
+	void deleteUser(int id);
+
+	Integer getUserCount();
+
+	List<User> getUserByPojo(User user);
 
 	//List<User> getUserByIds(QueryVo queryVo);
 
--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -6,7 +6,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author It扫地僧
@@ -31,6 +33,46 @@ public class TestExample {
         User user = userMapper.getUserById(2);
         String info = user.toString();
         System.out.println(info);
+    }
+
+    @Test
+    public void testSelectUserByUsername(){
+        List<User> users = userMapper.getUserByUserName("张");
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testUpdateUser(){
+        User user = new User();
+        user.setId(3);
+        user.setUsername("灭绝师太");
+        user.setGender("女");
+        user.setAddress("峨眉山");
+        userMapper.updateUser(user);
+        System.out.println("修改成功");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testDeleteUser(){
+        userMapper.deleteUser(4);
+        System.out.println("删除成功");
+    }
+
+    @Test
+    public void testGetUserCount(){
+        Integer userCount = userMapper.getUserCount();
+        System.out.println("用户数量为"+userCount);
+    }
+
+    //通过用户名或性别查询用户
+    @Test
+    public void testGetUserByPojo(){
+        User user = new User();
+        user.setUsername("张");
+        user.setGender("男");
+        List<User> users = userMapper.getUserByPojo(user);
+        users.forEach(System.out::println);
     }
     @After
     public void after(){
